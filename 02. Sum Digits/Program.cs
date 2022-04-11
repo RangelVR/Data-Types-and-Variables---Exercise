68998286eb66382ea20ca6f281e9af46175f5783
Rangel Rangelov <99962525+RangelVR@users.noreply.github.com>
2022-04-11 22:20:03 +0300

    Add files via upload

---
+++ 02. Sum Digits/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _02._Sum_Digits
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string num = Console.ReadLine();
+            int sum = 0;
+
+            for (int i = 0; i < num.Length; i++)
+            {
+                int currDigit = (int)(num[i]) - '0';
+                sum += currDigit;
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
