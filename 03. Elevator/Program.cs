d7777f59eb1fa8091666fadeb7a6e89441c68170
Rangel Rangelov <99962525+RangelVR@users.noreply.github.com>
2022-04-11 22:34:52 +0300

    Add files via upload

---
+++ 03. Elevator/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _03._Elevator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double numOfPeople = double.Parse(Console.ReadLine());
+            double capacityOfElevator = double.Parse(Console.ReadLine());
+
+            double courses = (numOfPeople / capacityOfElevator);
+
+            Console.WriteLine(courses);
+        }
+    }
+}
