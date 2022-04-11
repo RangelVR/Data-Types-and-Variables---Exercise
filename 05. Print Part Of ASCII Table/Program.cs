d92017033b313dd683e47f34073324ab5dd2e21e
Rangel Rangelov <99962525+RangelVR@users.noreply.github.com>
2022-04-11 23:06:10 +0300

    Add files via upload

---
+++ 05. Print Part Of ASCII Table/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _05._Print_Part_Of_ASCII_Table
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int startIndex = int.Parse(Console.ReadLine());
+            int stopIndex = int.Parse(Console.ReadLine());
+
+            for (int i = startIndex; i <= stopIndex; i++)
+            {
+                Console.Write($"{(char)i} ");
+            }
+        }
+    }
+}
