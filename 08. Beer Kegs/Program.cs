2114b35c679e288fee099631e2fc9f09f74be81c
Rangel Rangelov <99962525+RangelVR@users.noreply.github.com>
2022-04-13 00:23:52 +0300

    Add files via upload

---
+++ 08. Beer Kegs/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _08._Beer_Kegs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int countOfKegs = int.Parse(Console.ReadLine());
+            double biggestVolumeOfKeg = 0;
+            string biggestKeg = string.Empty;
+
+            for (int i = 0; i < countOfKegs; i++)
+            {
+                string typeOfKeg = Console.ReadLine();
+                double radiusOfKeg = double.Parse(Console.ReadLine());
+                double heightOfKeg = double.Parse(Console.ReadLine());
+
+                //π* r^2 * h.
+                double currVolume = Math.PI * Math.Pow(radiusOfKeg, 2) * heightOfKeg;
+                if (currVolume > biggestVolumeOfKeg)
+                {
+                    biggestVolumeOfKeg += currVolume;
+                    biggestKeg = typeOfKeg;
+                }
+            }
+
+            Console.WriteLine(biggestKeg);
+        }
+    }
+}
