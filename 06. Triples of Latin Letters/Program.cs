204ef04a3b03ee9460fcd35f8731542fa50d6870
Rangel Rangelov <99962525+RangelVR@users.noreply.github.com>
2022-04-12 22:35:02 +0300

    Add files via upload

---
+++ 06. Triples of Latin Letters/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _06._Triples_of_Latin_Letters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            string result = string.Empty;
+
+            for (int i = 0; i < num; i++)
+            {
+                char firstChar = (char)('a' + i);
+
+                for (int j = 0; j < num; j++)
+                {
+                    char secondChar = (char)('a' + j);
+
+                    for (int k = 0; k < num; k++)
+                    {
+                        char thirdChar = (char)('a' + k);
+                        Console.Write(firstChar);
+                        Console.Write(secondChar);
+                        Console.Write(thirdChar);
+                        Console.WriteLine();
+                    }
+                    
+                }
+            }
+        }
+    }
+}
