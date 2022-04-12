80959f7d1b66edd089fc96554f25d282d6e1c9ff
Rangel Rangelov <99962525+RangelVR@users.noreply.github.com>
2022-04-12 23:10:49 +0300

    Add files via upload

---
+++ 07. Water Overflow/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _07._Water_Overflow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = int.Parse(Console.ReadLine());
+            int tankCapacity = 255;
+            int littersInTheTank = 0;
+
+            for (int i = 0; i < count; i++)
+            {
+                int pouredLitters = int.Parse(Console.ReadLine());
+
+                if ((pouredLitters + littersInTheTank) > tankCapacity)
+                {
+                    Console.WriteLine("Insufficient capacity!");
+                }
+                else
+                {
+                    littersInTheTank += pouredLitters;
+                }
+                
+            }
+
+            Console.WriteLine(littersInTheTank);
+            
+        }
+    }
+}
