3408ecb4f02da600ab3c817f9148a291da84bcc3
Rangel Rangelov <99962525+RangelVR@users.noreply.github.com>
2022-04-13 23:42:42 +0300

    Add files via upload

---
+++ 10. Poke Mon/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _10._Poke_Mon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int pokePower = int.Parse(Console.ReadLine());
+            int distance = int.Parse(Console.ReadLine());
+            int exhaustionFactor = int.Parse(Console.ReadLine());
+            int targetsCount = 0;
+            double percent = (double)pokePower * 0.50;
+            while (pokePower >= distance)
+            {
+                
+                pokePower -= distance;
+
+                if (pokePower == percent && exhaustionFactor != 0)
+                {
+                    pokePower /= exhaustionFactor;
+                }
+
+                targetsCount++;
+            }
+            Console.WriteLine(pokePower);
+            Console.WriteLine(targetsCount);
+
+        }
+    }
+}
