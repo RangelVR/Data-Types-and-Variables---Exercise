c818034932cae43a9c9533926960532bfad03dac
Rangel Rangelov <99962525+RangelVR@users.noreply.github.com>
2022-04-13 22:19:15 +0300

    Add files via upload

---
+++ 09. Spice Must Flow/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _09._Spice_Must_Flow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int startingYield = int.Parse(Console.ReadLine());
+            int totalAmount = 0;
+            int days = 0;
+
+            while (startingYield >= 100)
+            {
+                totalAmount += startingYield;
+                startingYield -= 10;
+                days++;
+                
+            }
+            if (days > 0)
+            {
+                totalAmount -= days * 26;
+                totalAmount -= 26;
+            }
+
+            Console.WriteLine(days);
+            Console.WriteLine(totalAmount);
+        }
+    }
+}
