a19b3632dc4a39371832843d620ebf27e1873647
Rangel Rangelov <99962525+RangelVR@users.noreply.github.com>
2023-01-24 22:12:05 +0200

    Update Program.cs

--- 02. Sum Digits/Program.cs
+++ 02. Sum Digits/Program.cs
@@ -8,3 +8,16 @@ while (number > 0)
 }
 
 Console.WriteLine(sum);
+
+-------------------------------------------------------------------
+    
+//string num = Console.ReadLine();
+//int sum = 0;
+
+//for (int i = 0; i < num.Length; i++)
+//{
+//    int currNum = num[i];
+//    sum += currNum - '0';
+//}
+
+//Console.WriteLine(sum);
