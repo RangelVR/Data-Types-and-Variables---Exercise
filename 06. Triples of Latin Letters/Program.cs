5885144f2f5aa605f6a5176068c8b6eec6612025
Rangel Rangelov <99962525+RangelVR@users.noreply.github.com>
2023-01-24 22:55:08 +0200

    Update Program.cs

--- 06. Triples of Latin Letters/Program.cs
+++ 06. Triples of Latin Letters/Program.cs
@@ -1,25 +1,51 @@
-using System;
+int num = int.Parse(Console.ReadLine());
 
-namespace _06._Triples_of_Latin_Letters
+for (char i = 'a'; i < 'a' + num; i++)
 {
-    class Program
+    for (char j = 'a'; j < 'a' + num; j++)
     {
-        static void Main(string[] args)
+        for (char k = 'a'; k < 'a' + num; k++)
         {
-            int num = int.Parse(Console.ReadLine());
-
-            for (char i = 'a'; i <'a' + num; i++)
-            {
-                for (char j = 'a'; j <'a' + num; j++)
-                {
-                    for (char k = 'a'; k <'a' + num; k++)
-                    {
-                        Console.Write($"{i}{j}{k}");
-                        Console.WriteLine();
-                    }
-                    
-                }
-            }
+            Console.Write($"{i}{j}{k}");
+            Console.WriteLine();
         }
+
     }
 }
+-------------------------------------------------------------------------
+
+//int n = int.Parse(Console.ReadLine());
+
+//for (int i = 0; i < n; i++)
+//{
+//	for (int j = 0; j < n; j++)
+//	{
+//		for (int k = 0; k < n; k++)
+//		{
+//			char ch1 = (char)('a' + i);
+//			char ch2 = (char)('a' + j);
+//			char ch3 = (char)('a' + k);
+
+//			Console.Write($"{ch1}{ch2}{ch3}");
+//			Console.WriteLine();
+//		}
+
+//	}
+//}
+-------------------------------------------------------------------------
+
+//char ch = char.Parse(Console.ReadLine());
+//int stopCh = ch + '0';
+
+//for (char i = 'a'; i <= stopCh; i++)
+//{
+//    for (char j = 'a'; j <= stopCh; j++)
+//    {
+//        for (char k = 'a'; k <= stopCh; k++)
+//        {
+//            Console.Write($"{(char)(i)}{(char)(j)}{(char)(k)}");
+//            Console.WriteLine();
+//        }
+
+//    }
+//}
