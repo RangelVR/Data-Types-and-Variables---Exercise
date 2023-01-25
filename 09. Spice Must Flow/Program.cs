b3a98013cc2efe6fd572615435a0d7d61b28ab72
Rangel Rangelov <99962525+RangelVR@users.noreply.github.com>
2023-01-25 21:38:58 +0200

    Update Program.cs

--- 09. Spice Must Flow/Program.cs
+++ 09. Spice Must Flow/Program.cs
@@ -1,28 +1,41 @@
-using System;
+int startingYield = int.Parse(Console.ReadLine());
+int totalYield = 0;
+int crewConsumption = 26;
+int daysMining = 0;
 
-namespace _09._Spice_Must_Flow
+while (startingYield >= 100)
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            double yield = double.Parse(Console.ReadLine());
-            double totalSpice = 0;
-            int day = 0;
+    int dayYeld = startingYield - crewConsumption;
+    totalYield += dayYeld;
 
-            if (yield >= 100)
-            {
-                for (double i = yield; i >= 100; i -= 10)
-                {
-                    double spice = i;
-                    spice -= 26;
-                    totalSpice += spice;
-                    day++;
-                }
-                totalSpice -= 26;
-            }
-            Console.WriteLine(day);
-            Console.WriteLine(totalSpice);
-        }
-    }
+    startingYield -= 10;
+    daysMining++;
 }
+
+if (totalYield >= crewConsumption)
+{
+    totalYield -= crewConsumption;
+}
+
+Console.WriteLine(daysMining);
+Console.WriteLine(totalYield);
+
+---------------------------------------------------------------------------
+//int yield = int.Parse(Console.ReadLine());
+//int totalYield = 0;
+//int day = 0;
+
+//for (int i = yield; i >= 100; i -= 10)
+//{
+//    int yieldDay = i - 26;
+//    totalYield += yieldDay;
+//    day++;
+//}
+//if (totalYield >= 26)
+//{
+//    totalYield -= 26;
+//}
+
+
+//Console.WriteLine(day);
+//Console.WriteLine(totalYield);
